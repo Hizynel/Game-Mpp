9f51bee722c8d7db7995964fb1f8c10f3660cde3
Hizynel <hizbadestayana8@gmail.com>
2026-08-19 09:17:28 +0700

    gameplay

--- Script/tutorial.gd
+++ Script/tutorial.gd
@@ -6,47 +6,18 @@ extends Control
 
 
 func _ready() -> void:
-	# Awalnya background hitam
-	bg_tuto.modulate = Color(0, 0, 0, 1)
+	# Langsung tampil normal
+	bg_tuto.modulate = Color(0.3, 0.3, 0.3, 1)
+	label.modulate.a = 1.0
+	paham_button.modulate.a = 1.0
 
-	# Label dan button awalnya transparan
-	label.modulate.a = 0.0
-	paham_button.modulate.a = 0.0
-
-	# Hubungkan tombol
 	paham_button.pressed.connect(_on_paham_pressed)
-
-	# Fade IN awal
-	var tween = create_tween()
-	tween.set_parallel(true)
-
-	tween.tween_property(
-		bg_tuto,
-		"modulate",
-		Color(0.3, 0.3, 0.3, 1),
-		4.0
-	)
-
-	tween.tween_property(
-		label,
-		"modulate:a",
-		1.0,
-		4.0
-	)
-
-	tween.tween_property(
-		paham_button,
-		"modulate:a",
-		1.0,
-		4.0
-	)
 
 
 func _on_paham_pressed() -> void:
-	# Supaya tombol tidak bisa ditekan berkali-kali
 	paham_button.disabled = true
 
-	# Fade OUT sekali
+	# Fade ke hitam
 	var tween = create_tween()
 	tween.set_parallel(true)
 
@@ -71,12 +42,8 @@ func _on_paham_pressed() -> void:
 		1.0
 	)
 
-	# Tunggu sampai fade selesai
+	# Tunggu sampai layar hitam
 	await tween.finished
 
 	# Baru pindah scene
-	_on_tutorial_selesai()
-
-
-func _on_tutorial_selesai() -> void:
 	get_tree().change_scene_to_file("res://NAMA_SCENE_GAME.tscn")
